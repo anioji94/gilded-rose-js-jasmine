fc23d2839fe28c5c8688f909425ebbd732925d81
Aniruddh <ojhaa94@gmail.com>
2021-04-08 17:30:50 +0100

    adds Aged Brie

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,12 +1,13 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
-
+	beforeEach(function() {
+		gildedRose = new Shop([ new Item("foo", 1, 1) ]);
+	});
 
 	
 	describe("Item", function() {
 
 		it("should foo", function() {
-			const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
 			const items = gildedRose.updateQuality();
 			expect(items[0].name).toEqual("foo");
 		});
@@ -15,17 +16,21 @@ describe("Gilded Rose", function() {
 	describe("Shop", function() {
 		describe("#updateQuality()", function() {
 			it("should deplete sellIn value every day", function() {
-				const gildedRose = new Shop([ new Item("foo", 1, 1) ]);
 				const items = gildedRose.updateQuality();
 				expect(items[0].sellIn).toEqual(0)
 			});
 
 			it("should deplete Quality score every day", function() {
-				const gildedRose = new Shop([ new Item("foo", 1, 1) ]);
 				const items = gildedRose.updateQuality();
 				expect(items[0].quality).toEqual(0)
 			});
 
+			it("should increase the quality of 'Aged Brie' daily", function() {
+				let brie = new Item("Aged Brie", 1, 1)
+				gildedRose.items.push(brie);
+				const items = gildedRose.updateQuality();
+				expect(items[1].quality).toEqual(2)
+			})
 
 		})
 	});
